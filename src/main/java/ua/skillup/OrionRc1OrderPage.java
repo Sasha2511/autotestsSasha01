4dcc128ef5c8153a17f63cc3a03cb5413f22c762
Sasha2511 <nedzelnytskyis@gmail.com>
2024-08-15 14:23:41 +0300

    OrderCreateGVVTest travelers creation 03

--- src/main/java/ua/skillup/OrionRc1OrderPage.java
+++ src/main/java/ua/skillup/OrionRc1OrderPage.java
@@ -4,8 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.devtools.v124.input.Input;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -16,11 +17,11 @@ public class OrionRc1OrderPage {
     private final WebDriver driver;
 
     private final Object[][] TRAVELERS = {
-            {1, "Tom", "Seaver", "01 Jan 1981", 1},
-            {2, "Emma", "Seaver", "01 Jan 1982", 2},
-            {1, "David", "Seaver", "01 Jan 1983", 1},
-            {2, "Sofy", "Seaver", "01 Jan 2016", 2},
-            {1, "David", "Seaver", "01 Jan 2017", 1},
+            {1, "Tom", "Seaver", "01 Jan 1981", 0},
+            {2, "Emma", "Seaver", "01 Jan 1982", 1},
+            {1, "David", "Seaver", "01 Jan 1983", 0},
+            {2, "Sofy", "Seaver", "01 Jan 2016", 1},
+            {1, "David", "Seaver", "01 Jan 2017", 0},
     };
 
     public OrionRc1OrderPage(WebDriver driver) {
@@ -29,14 +30,31 @@ public class OrionRc1OrderPage {
 
     public void open() {
         driver.get("http://secret:tsdebug@rc1-orion.test.greatvaluevacations.com/admin/");
-        driver.manage().addCookie(new Cookie.Builder("orion_rc_1_session", "eyJpdiI6ImpFUTJPcW5OU0x3aUdZRWhGSytVT3c9PSIsInZhbHVlIjoiVXRTcHhoWXdpNmlxdHFlSEk3WDlZeCtqWUprRDVhRkxVbCtROUV0TVNxZ1NPclZ2R1hrS0dIL1J1SDBNV1BGeEQ1aTVjMzNyUVZML3NNNENJRVM1TDg0bmd5cE9wWUdteVkrUFJBdUV3bU5lWDVlb3dCVXg2VDZHQ0J1TzNSYWUiLCJtYWMiOiJjOGVkODRhNjAxYzQyYjJhY2JlMDM4ZmZlYmQ2ZDg3NTVjNTI1OWQzMWFkODkxOTg1MDAwOWRkODViYWYyMjQ1IiwidGFnIjoiIn0%3D").build());
+        driver.manage().addCookie(new Cookie.Builder("orion_rc_1_session", "eyJpdiI6IlpDMEVYbVNKZDR1by80bEhXSEpxa1E9PSIsInZhbHVlIjoiTGFJeTBSWDJjelk5SSt5RzVibzByMkNsc2FDUk1lQUhISlF0UGt1c0RlZEoxUGszNlg4WG9sMFlGZ0tQTzFqN3RMK2hPRkZ6eFRERUhoOFVwZFJDMDNiVm5FUUR1akFnY3R1RzFmVkpmd2FqdGdvRk4yajhIZVpsYkQvd1NVd1EiLCJtYWMiOiJjNGQ3YzAyM2RhZjJjYmJkZmU4Y2U0OThmMzcyYzdlMmIyMDdhY2Q3ODI5YjcyNjhjNjE3YTQ2N2JhYmM1ZWZiIiwidGFnIjoiIn0%3D").build());
         driver.get("http://rc1-orion.test.greatvaluevacations.com/admin/order/DEVG315#summary");
+    }
+
+    private String[] getTravelerXpathByLine(int n) {
+        String[] travelerLineArray;
+        travelerLineArray = new String[]{
+                "//tbody/tr[" + n + "]//select[@class='custom-select traveler__select traveler__select--prefix']",
+                "//tbody/tr[" + n + "]//input[@class='form-control traveler__name-input']",
+                "//tbody/tr[" + n + "]/td[5]/input[@class='form-control traveler__name-input']",
+                "//tbody/tr[" + n + "]//div[@class='traveler__dob-input']//input",
+                "//tbody/tr[" + n + "]//select[@class='custom-select traveler__select traveler__select--gender']"
+        };
+        return travelerLineArray;
     }
 
     public void setTravelers() {
 
         final By FIRST_TRAVELER_PREFIX = By.xpath("//tbody/tr[1]//select[@class='custom-select traveler__select traveler__select--prefix']");
 
+        By traveler_PREFIX;
+        By traveler_NAME;
+        By traveler_LASTNAME;
+        By traveler_DOB;
+        By traveler_GENDER;
 
         WebElement travelersTab = driver.findElement(TRAVELERS_TAB);
         travelersTab.click();
@@ -47,6 +65,22 @@ public class OrionRc1OrderPage {
         Select firstTravelerPrefix = new Select(firstTravelerPrefixDropdown);
 
         firstTravelerPrefix.selectByIndex(2);
+
+     /*   for (int i=0; i<=4; i++) {
+            String[] line =  getTravelerXpathByLine(i+1);
+            traveler_PREFIX = By.xpath(line[0]);
+            traveler_NAME = By.xpath(line[1]);
+
+
+            Select travelerPrefix = new Select((WebElement) traveler_PREFIX);
+           // WebElement inputName = driver.findElement(traveler_NAME);
+
+            travelerPrefix.selectByIndex((int) TRAVELERS[i][0]);
+            //inputName.sendKeys((String) TRAVELERS[i][1]);
+        }
+
+
+      */
 
     }
 }
